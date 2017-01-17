a912f993bc26b26afaec766f2af6321210d3a147
sujiang <jimmysu1991@gmail.com>
2017-01-17 13:44:15 +0800

    added is_admin column to users and migrated

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,6 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
          def admin?
-           email == 'a@b.com'
+           is_admin
          end
 end
