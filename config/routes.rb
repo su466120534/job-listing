54b424a8ab264f3ffc616a5c457c1ef136e66461
sujiang <jimmysu1991@gmail.com>
2017-01-18 10:01:56 +0800

    did resume controller, now it is able to send resume

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-   resources :jobs
-
-     namespace :admin do
-      resources :jobs
-     end
 
      namespace :admin do
        resources :jobs do
@@ -15,6 +10,10 @@ Rails.application.routes.draw do
          end
        end
      end
-     
+
+     resources :jobs do
+       resources :resumes
+     end
+
    root 'jobs#index'
  end
